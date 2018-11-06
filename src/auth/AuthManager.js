76acac2a7e2002866b60d910135ec28e763a16f4
Lars Bärtschi <lars.baertschi@iet-gibb.ch>
2018-11-06 15:02:11 +0100

    Add @material-ui/core, decode jwt token

--- src/auth/AuthManager.js
+++ src/auth/AuthManager.js
@@ -1,7 +1,10 @@
+import jwtdecode from 'jwt-decode';
+
 if (!window.signin) {
     console.log('Waiting for JWT...');
     window.signin = jwt => {
         console.log('JWT: ' + jwt);
+        manager.authenticate(jwt);
     }
 }
 
@@ -9,6 +12,18 @@ class AuthManager {
     constructor() {
         this.isAuthenticated = false;
     }
+
+    isAuthenticated() {
+        return this.isAuthenticated;
+    }
+
+    authenticate(jwt) {
+        this.jwt = jwt;
+        this.userByJwt = jwtdecode(jwt);
+        console.log(this.userByJwt);
+    }
 }
 
-export default new AuthManager();
+const manager = new AuthManager();
+
+export default manager;
