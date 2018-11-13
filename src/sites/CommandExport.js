b9bd226c0c40734e31faeaab652a68dd016c8f8f
Yvo <yvo.andri.hofer@iet-gibb.ch>
2018-11-13 16:18:48 +0100

    + example API request

--- src/sites/CommandExport.js
+++ src/sites/CommandExport.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import APIConnector from '../api/APIConnector';
 
 class CommandExport extends Component {
+
+    constructor(props) {
+        super(props);
+        this.remote = window.require("electron").remote;
+    }
+
+    exportCommands() {
+        APIConnector.ready(() => {
+            APIConnector.doPanelRequest("plugins{isInstalled, name, author, version, description, commands, permissions}")
+                .then(result => {console.log(result)})
+        })
+    }
+
     render() {
         return (
-            <p>HELLO IM THE COMMANDEXPORT</p>
+            <button onClick={this.exportCommands}>Export</button>
         );
     }
 }
