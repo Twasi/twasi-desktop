f337e9c9ed046bdeb5beefb7dc9e1e234877d21f
yvo2 <yvo.a.hofer@bluewin.ch>
2018-11-15 22:20:19 +0100

    + impl command export

--- src/sites/CommandExport.js
+++ src/sites/CommandExport.js
@@ -1,17 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import APIConnector from '../api/APIConnector';
+
 class CommandExport extends Component {
 
     constructor(props) {
         super(props);
         this.remote = window.require("electron").remote;
+
     }
 
     exportCommands() {
         APIConnector.ready(() => {
+
             APIConnector.doPanelRequest("commands{id, name, content}", "commands")
-                .then(result => {console.log(result)})
+                .then(result => {
+
+                    const app = window.require('electron').remote;
+                    const dialog = app.dialog;
+                    const fs = window.require('fs');
+
+                    dialog.showSaveDialog({
+                        filters: [{
+                            name: 'Text Documents (*.txt)',
+                            extensions: ['txt']
+                        }]
+                    }, (fileName) => {
+                        if (fileName === undefined) {
+                            console.log("You didn't save the file");
+                            return;
+                        }
+
+                        fs.writeFile(fileName, JSON.stringify(result), (err) => {
+                            if (err) {
+                                alert("An error ocurred creating the file " + err.message)
+                            }
+
+                            alert("The file has been succesfully exported");
+
+                        });
+                    });
+                })
         });
+
     }
 
     render() {
