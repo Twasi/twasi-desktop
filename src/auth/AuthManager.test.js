ff2d7b68176d1c163ecee1a96be3ba7711de8af1
Lars Bärtschi <lars.baertschi@iet-gibb.ch>
2018-11-20 14:27:47 +0100

    add ;

--- src/auth/AuthManager.test.js
+++ src/auth/AuthManager.test.js
@@ -25,6 +25,6 @@ describe('AuthManager', () => {
         auth.authenticate("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c");
 
         expect(auth.userByJwt.name).toBe("John Doe");
-    })
+    });
 
 });
