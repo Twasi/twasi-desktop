b42084009dee2f2122e71d483ac8b55ab19ab8ef
Cyril Heiniger <cyrilheiniger@Cyrils-MacBook-Pro.local>
2018-11-20 14:26:46 +0100

    songsync design

--- src/sites/SongSync.js
+++ src/sites/SongSync.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import Switch from '@material-ui/core/Switch';
+import APIConnector from "../api/APIConnector";
 
 class SongSync extends Component {
+
+    songSync() {
+
+
+    }
     render() {
         return (
             <div>
                 <p>HELLO IM THE Songsnyc XD</p>
-                <Switch />
+                <Switch className="Switchsongsync"/>
+                <button onClick={this.songSync}>Sync</button>
             </div>
         );
     }
