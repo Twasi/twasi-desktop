92beee4381c5f4bd2913cd45dd629db3a1dec086
yvo2 <yvo.a.hofer@bluewin.ch>
2018-11-20 21:37:29 +0100

    + implement SongSync v1

--- src/sites/SongSync.js
+++ src/sites/SongSync.js
@@ -1,9 +1,64 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import APIConnector from '../api/APIConnector';
 import Switch from '@material-ui/core/Switch';
 
 class SongSync extends Component {
 
+    constructor(props) {
+
+        super(props);
+        this.state = {isDisabled: true};
+        this.toggleButton = this.toggleButton.bind(this);
+
+    }
+
+    toggleButton(e, checked) {
+
+        if (checked) {
+            this.setState({isDisabled: !checked});
+        } else {
+            this.setState({isDisabled: !checked})
+        }
+
+    }
+
     songSync() {
+
+        const app = window.require('electron').remote;
+        const fs = window.require('fs');
+        const dialog = app.dialog;
+
+        dialog.showOpenDialog({
+            properties: ['openDirectory']
+        }, function (folderPath) {
+
+            if (folderPath === undefined) {
+                console.log("You didn't choose a folder");
+            } else {
+
+                // do all 5 seconds until button is toggled & replace with current song when implemented
+
+                APIConnector.ready(() => {
+
+                    APIConnector.doPanelRequest("commands{id, name, content}", "commands")
+                        .then(result => {
+
+                            let commandToExport = result.commands[0].id + "-" + result.commands[0].name + "-" + result.commands[0].content;
+
+                            fs.writeFile(folderPath[0] + '\\SongExport.txt', commandToExport, (err) => {
+
+                                if (err) {
+                                    alert("An error occurred creating the file " + err.message)
+                                }
+
+                            });
+
+                        })
+                });
+
+            }
+
+        });
 
     }
 
@@ -11,8 +66,9 @@ class SongSync extends Component {
         return (
             <div>
                 <p>Welcome to SongSync</p>
-                <Switch className="Switchsongsync"/>
-                <button onClick={this.songSync}>Sync</button>
+                <Switch onChange={this.toggleButton} className="syncSwitch"/>
+                <button onClick={this.songSync} className={"syncButton"} disabled={this.state.isDisabled}>choose path
+                </button>
             </div>
         );
     }
