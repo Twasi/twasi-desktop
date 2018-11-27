211ad24ff148f061a4c96ee035e646620fe9f757
Lars Bärtschi <lars.baertschi@iet-gibb.ch>
2018-11-27 15:02:51 +0100

    fix directory path

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -15,11 +15,7 @@ function createWindow() {
     app.mainWindow = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    const startUrl = process.env.ELECTRON_START_URL || url.format({
-            pathname: path.join(__dirname, '/../build/index.html'),
-            protocol: 'file:',
-            slashes: true
-        });
+    const startUrl = process.env.ELECTRON_START_URL || `file://${__dirname}/../build/index.html`;
     app.mainWindow.loadURL(startUrl);
 
     // Open the DevTools.
