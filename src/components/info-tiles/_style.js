d9c88201198a03b4bc6e38ef8a059a0b470318c9
Cyril Heiniger <cyrilheiniger@Cyrils-MacBook-Pro.local>
2018-12-04 13:22:22 +0100

    playing with the style

--- src/components/info-tiles/_style.js
+++ src/components/info-tiles/_style.js
@@ -1,6 +1,6 @@
 export const getListStyle = () => ({
     display: 'block',
-    width: '200px',
+    width: '300px',
     boxShadow: 'none'
 });
 
@@ -13,13 +13,14 @@ export const getTitleStyle = () => ({
 export const getItemStyle = () => ({
     backgroundColor: '#1b292c',
     fontSize: 12,
-    fontWeight: '600',
-    lineHeight: '48px',
+    //marginTop: 10,
+    fontWeight: '700',
+    lineHeight: '120px',
     color: '#fff',
     marginLeft: 0,
-    padding: '0px 16px 0px 16px',
+    padding: '10px 10px 10px 10px',
     minHeight: 48,
     position: 'relative',
-    textAlign: 'center',
-    borderTop: '1px solid #00aeae'
+     textAlign: 'center',
+    //borderTop: '0px solid #00aeae'
 });
